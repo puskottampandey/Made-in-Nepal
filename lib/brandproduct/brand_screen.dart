8c38f64841023a3f038b3a754c32445efec2fc2a
puskottampandey <puskottam.pandey@gmail.com>
2023-03-20 16:25:03 +0545

    Brand product Details screen

--- lib/brandproduct/brand_screen.dart
+++ lib/brandproduct/brand_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:madeinnepal/brandproduct/productdetails_screen.dart';
 import 'package:madeinnepal/constant.dart';
 import 'package:madeinnepal/model_screen/brand.dart';
 
@@ -31,69 +32,93 @@ class Brands extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: Stack(
                     children: [
-                      Container(
-                        width: 175,
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 0.0, horizontal: 8),
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              spreadRadius: 0.0,
-                              blurRadius: 2,
-                            )
-                          ],
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              brand.products![index].name.toString(),
-                              style: const TextStyle(
-                                fontSize: 15,
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(3.0),
-                                  child: Text(
-                                    brand.products![index].price.toString(),
-                                    style: const TextStyle(
-                                        color: Colors.blueGrey,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ProductDetails(
+                                        products: brand.products![index],
+                                      )));
+                        },
+                        child: Container(
+                          width: 175,
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 0.0, horizontal: 8),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                spreadRadius: 0.0,
+                                blurRadius: 2,
+                              )
+                            ],
+                            borderRadius: BorderRadius.all(Radius.circular(8)),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                brand.products![index].name.toString(),
+                                style: const TextStyle(
+                                  fontSize: 15,
                                 ),
-                                Text(
-                                  brand.products![index].lastprice.toString(),
-                                  style: const TextStyle(
-                                      fontSize: 10,
-                                      decoration: TextDecoration.lineThrough,
-                                      decorationThickness: 1),
-                                )
-                              ],
-                            ),
-                          ],
+                              ),
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(3.0),
+                                    child: Text(
+                                      brand.products![index].price.toString(),
+                                      style: const TextStyle(
+                                          color: Colors.blueGrey,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                  ),
+                                  Text(
+                                    brand.products![index].lastprice.toString(),
+                                    style: const TextStyle(
+                                        fontSize: 10,
+                                        decoration: TextDecoration.lineThrough,
+                                        decorationThickness: 1),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       Positioned(
                         right: 8,
                         top: 0.1,
-                        child: Container(
-                          height: 175,
-                          width: 175,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(8)),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadiusDirectional.circular(8),
-                            child: Image.network(
-                              brand.products![index].imageurl.toString(),
-                              fit: BoxFit.cover,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ProductDetails(
+                                          products: brand.products![index],
+                                        )));
+                          },
+                          child: Container(
+                            height: 175,
+                            width: 175,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(8)),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadiusDirectional.circular(8),
+                              child: Hero(
+                                tag: Key,
+                                child: Image.network(
+                                  brand.products![index].imageurl.toString(),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
                           ),
                         ),
